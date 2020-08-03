8baeb18df4d4c0e7aecec4e9c24f33cbe314a112
arsvechkarev <arsvechkarev@gmail.com>
2020-08-03 14:59:04 +0300

    Added touch slop handling to ClickAndHoldIconButton.kt

--- app/src/main/java/com/chemistry/calculator/views/keyboard/ClickAndHoldIconButton.kt
+++ app/src/main/java/com/chemistry/calculator/views/keyboard/ClickAndHoldIconButton.kt
@@ -2,13 +2,18 @@ package com.chemistry.calculator.views.keyboard
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.PointF
 import android.os.Handler
 import android.os.Message
 import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_CANCEL
 import android.view.MotionEvent.ACTION_DOWN
+import android.view.MotionEvent.ACTION_MOVE
 import android.view.MotionEvent.ACTION_UP
+import android.view.ViewConfiguration
 import com.chemistry.calculator.utils.postDelayed
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.math.hypot
 
 @SuppressLint("ViewConstructor") // Created only through code
 class ClickAndHoldIconButton constructor(
@@ -21,9 +26,11 @@ class ClickAndHoldIconButton constructor(
   private val onHold: (String) -> Unit
 ) : IconButton(context, id, iconRes, iconColor, backgroundColor, onClicked = onClicked) {
   
-  private var isHoldingNow = AtomicBoolean(false)
-  
   private val initiatorHandler = Handler()
+  private val touchSlop = ViewConfiguration.get(context).scaledTouchSlop
+  
+  private var isHoldingNow = AtomicBoolean(false)
+  private var lastDownPoint = PointF()
   
   @SuppressLint("HandlerLeak")
   private val holdHandler = object : Handler() {
@@ -45,15 +52,26 @@ class ClickAndHoldIconButton constructor(
     when (event.action) {
       ACTION_DOWN -> {
         isHoldingNow.set(true)
+        lastDownPoint.set(event.x, event.y)
         initiatorHandler.postDelayed(DELAY_START_HOLDING) { holdHandler.sendEmptyMessage(WHAT) }
       }
-      ACTION_UP -> {
-        isHoldingNow.set(false)
-        holdHandler.sendEmptyMessage(WHAT)
-        initiatorHandler.removeCallbacksAndMessages(null)
+      ACTION_MOVE -> {
+        val distX = event.x - lastDownPoint.x
+        val distY = event.y - lastDownPoint.y
+        val dist = hypot(distX, distY)
+        if (dist > touchSlop) {
+          cancelHolding()
+        }
       }
+      ACTION_UP, ACTION_CANCEL -> cancelHolding()
     }
     return super.onTouchEvent(event)
+  }
+  
+  private fun cancelHolding() {
+    isHoldingNow.set(false)
+    holdHandler.sendEmptyMessage(WHAT)
+    initiatorHandler.removeCallbacksAndMessages(null)
   }
   
   override fun onDetachedFromWindow() {
